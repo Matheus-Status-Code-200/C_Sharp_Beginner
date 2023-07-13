b6bc783b1cb466fb5c42a13a707eef4ca35d9bd9
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-13 19:09:05 -0300

    Primeiro Commit

---
+++ Aula_01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(2/ 2);
+        }
+    }
+}
---
+++ Aula_02/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World 2!");
+Console.WriteLine(2+2);
+Console.WriteLine(2/2);
+Console.WriteLine(2**2);
+Console.WriteLine(2>2);
