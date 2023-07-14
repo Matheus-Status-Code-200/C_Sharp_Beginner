cff1b5a182b446c04da4228f77f29f640ffaa0c2
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-14 09:25:07 -0300

    If Else Manipulando String

---
+++ Aula_05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite um nome: ");
+string nome = Console.ReadLine();
+
+if (string.IsNullOrEmpty(nome))
+    {
+        Console.WriteLine("O nome está vazio!");
+    }
+else
+    {
+        int quantidadeLetras = nome.Length;
+        Console.WriteLine("A quantidade de letras no nome é: " + quantidadeLetras);
+    }
