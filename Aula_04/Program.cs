851a9feefe7db441709a5421664f2cb5d3872f3a
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-14 09:16:21 -0300

    Manipulando String

---
+++ Aula_04/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Vamos manipular uma String.");
+Console.Write("Digite um nome: ");
+string nome = Console.ReadLine();
+
+int quantidadeLetras = nome.Length;
+Console.WriteLine("A quantidade de letras no nome é: " + quantidadeLetras);
