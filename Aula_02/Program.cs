753655d87ebfcef6d147f2a32b0ef637b30b2033
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-13 22:00:00 -0300

    update

--- Aula_02/Program.cs
+++ Aula_02/Program.cs
@@ -1,25 +1,24 @@
-        ﻿Console.WriteLine("Soma 2 + 2: " + (2 + 2));
-        Console.WriteLine("Subtração 5 - 3: " + (5 - 3));
-        Console.WriteLine("Multiplicação 2 * 3: " + (2 * 3));
-        Console.WriteLine("Divisão 10 / 2: " + (10 / 2));
-        Console.WriteLine("Módulo 10 % 3: " + (10 % 3));
-        
-        int numero = 5;
-        Console.WriteLine("Incremento ++numero: " + (++numero));
-        int outroNumero = 10;
-        Console.WriteLine("Decremento --outroNumero: " + (--outroNumero));
+﻿Console.WriteLine("Operadores em C#.\n " );
+Console.WriteLine("Soma 2 + 2: " + (2 + 2));
+Console.WriteLine("Subtração 5 - 3: " + (5 - 3));
+Console.WriteLine("Multiplicação 2 * 3: " + (2 * 3));
+Console.WriteLine("Divisão 10 / 2: " + (10 / 2));
+Console.WriteLine("Módulo 10 % 3: " + (10 % 3)+"\n");
 
-        Console.WriteLine("Igualdade 5 == 5: " + (5 == 5));
-        Console.WriteLine("Diferença 5 != 3: " + (5 != 3));
-        Console.WriteLine("Menor que 2 < 5: " + (2 < 5));
-        Console.WriteLine("Maior que 5 > 2: " + (5 > 2));
-        Console.WriteLine("Menor ou igual a 2 <= 2: " + (2 <= 2));
-        Console.WriteLine("Maior ou igual a 5 >= 5: " + (5 >= 5));
+int numero = 5;
+Console.WriteLine("Incremento ++numero: " + (++numero));
+int outroNumero = 10;
+Console.WriteLine("Decremento --outroNumero: " + (--outroNumero)+"\n");
 
-        bool condicao1 = true;
-        bool condicao2 = false;
-        Console.WriteLine("E lógico condicao1 && condicao2: " + (condicao1 && condicao2));
-        Console.WriteLine("Ou lógico condicao1 || condicao2: " + (condicao1 || condicao2));
-        Console.WriteLine("Negação lógica !condicao1: " + (!condicao1));
+Console.WriteLine("Igualdade 5 == 5: " + (5 == 5));
+Console.WriteLine("Diferença 5 != 3: " + (5 != 3));
+Console.WriteLine("Menor que 2 < 5: " + (2 < 5));
+Console.WriteLine("Maior que 5 > 2: " + (5 > 2));
+Console.WriteLine("Menor ou igual a 2 <= 2: " + (2 <= 2));
+Console.WriteLine("Maior ou igual a 5 >= 5: " + (5 >= 5)+"\n");
 
-        Console.ReadLine();
+bool condicao1 = true;
+bool condicao2 = false;
+Console.WriteLine("E lógico condicao1 && condicao2: " + (condicao1 && condicao2));
+Console.WriteLine("Ou lógico condicao1 || condicao2: " + (condicao1 || condicao2));
+Console.WriteLine("Negação lógica !condicao1: " + (!condicao1));
