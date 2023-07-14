01b6edc30d3b2599440386f76fba36e91ff277e5
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-14 09:44:59 -0300

    Pega uma posição desejada

---
+++ Aula_06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um nome: ");
+        string nome = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(nome))
+        {
+            Console.WriteLine("O nome está vazio!");
+            Console.WriteLine("Fim do programa.");
+                return;
+        }
+        else
+        {
+            Console.Write($"Digite a posição do nome de 1 a {nome.Length} que deseja obter a letra: ");
+            int posicao = int.Parse(Console.ReadLine());
+
+            if (posicao >= 1 && posicao <= nome.Length)
+            {
+                char letra = nome[posicao - 1];
+                Console.WriteLine("A letra na posição " + posicao + " é: " + letra);
+                Console.WriteLine("Fim do programa.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Posição inválida!");
+                Console.WriteLine("Fim do programa.");
+                return;
+
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
+
