05a28e3dc37922b5da0cf957b23d95763c678301
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-13 21:52:39 -0300

    Operdores

--- Aula_02/Program.cs
+++ Aula_02/Program.cs
@@ -1,6 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World 2!");
-Console.WriteLine(2+2);
-Console.WriteLine(2/2);
-Console.WriteLine(2**2);
-Console.WriteLine(2>2);
+﻿Console.WriteLine("Soma 2 + 2: " + (2 + 2));
+
+        Console.WriteLine("Subtração 5 - 3: " + (5 - 3));
+        Console.WriteLine("Multiplicação 2 * 3: " + (2 * 3));
+        Console.WriteLine("Divisão 10 / 2: " + (10 / 2));
+        Console.WriteLine("Módulo 10 % 3: " + (10 % 3));
+        int numero = 5;
+        Console.WriteLine("Incremento ++numero: " + (++numero));
+        int outroNumero = 10;
+        Console.WriteLine("Decremento --outroNumero: " + (--outroNumero));
+
+        Console.WriteLine("Igualdade 5 == 5: " + (5 == 5));
+        Console.WriteLine("Diferença 5 != 3: " + (5 != 3));
+        Console.WriteLine("Menor que 2 < 5: " + (2 < 5));
+        Console.WriteLine("Maior que 5 > 2: " + (5 > 2));
+        Console.WriteLine("Menor ou igual a 2 <= 2: " + (2 <= 2));
+        Console.WriteLine("Maior ou igual a 5 >= 5: " + (5 >= 5));
+
+        bool condicao1 = true;
+        bool condicao2 = false;
+        Console.WriteLine("E lógico condicao1 && condicao2: " + (condicao1 && condicao2));
+        Console.WriteLine("Ou lógico condicao1 || condicao2: " + (condicao1 || condicao2));
+        Console.WriteLine("Negação lógica !condicao1: " + (!condicao1));
+
+        Console.ReadLine();
