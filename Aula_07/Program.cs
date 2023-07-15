6eb98c89c3ec84b3bac23ad54b6a745830723550
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-15 10:37:09 -0300

    Manipulando lista com For

---
+++ Aula_07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Manipulando lista com For: \n");
+        List<string> nomes = new List<string>
+        {
+            "João",
+            "Maria",
+            "Pedro",
+            "Ana",
+            "Carlos",
+            "Laura",
+            "Fernando",
+            "Lúcia",
+            "Mariana",
+            "Rafael"
+        };
+
+        for (int i = 0; i < nomes.Count; i++)
+        {
+            Console.WriteLine($"Nome na posição {i}: " + nomes[i]);
+        }
+        Console.WriteLine($"Fim, sua lista possui {nomes.Count} nomes.");
+      
+    }
+}
