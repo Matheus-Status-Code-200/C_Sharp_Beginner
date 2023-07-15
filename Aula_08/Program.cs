17710e65c9c9db10e594bbdd6ad07b534799736f
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-15 10:42:13 -0300

    Manipulando lista com Foreach

---
+++ Aula_08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> nomes = new List<string>
+        {
+            "João",
+            "Maria",
+            "Pedro",
+            "Ana",
+            "Carlos",
+            "Laura",
+            "Fernando",
+            "Lúcia",
+            "Mariana",
+            "Rafael"
+        };
+
+        Console.WriteLine("Manipulando lista com Foreach:");
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine("Nome: " + nome);
+        }
+
+        Console.WriteLine($"Fim, sua lista possui {nomes.Count} nomes.");
+        
+    }
+}
