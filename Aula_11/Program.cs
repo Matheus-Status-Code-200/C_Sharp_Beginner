080ab22cdefb5afbf1181c99cebfc088529ebc4d
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-15 11:13:42 -0300

    Programa de Conversão de Entrada.

---
+++ Aula_11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Programa de Conversão de Entrada");
+        Console.WriteLine("--------------------------------\n");
+
+        Console.Write("Digite algo: ");
+        string input = Console.ReadLine()?? string.Empty;
+
+        if (int.TryParse(input, out int numeroInteiro))
+        {
+            Console.WriteLine("Número inteiro digitado: " + numeroInteiro);
+        }
+        else if (float.TryParse(input, out float numeroFloat))
+        {
+            Console.WriteLine("Número de ponto flutuante digitado: " + numeroFloat);
+        }
+        else
+        {
+            Console.WriteLine("Você digitou um texto ou caractere especial.");
+        }
+    }
+}
