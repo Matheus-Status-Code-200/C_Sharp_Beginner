3e3d12b04e8e6da7d1433edf85a48413ae9ac5fc
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-15 10:54:26 -0300

    Manipulando lista e string.

---
+++ Aula_09/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> nomes = new List<string>
+        {
+            "João",
+            "Maria",
+            "Pedro",
+            "Ana",
+            "Carlos",
+            "Laura",
+            "Fernando",
+            "Lúcia",
+            "Mariana",
+            "Rafael"
+        };
+        Console.WriteLine("Manipulando lista e string.");
+        Console.Write("Digite um nome: ");
+        /* Operador de coalescência nula (??) para atribuir string.Empty (uma string vazia) 
+        à variável nomeBusca caso Console.ReadLine() retorne null. 
+        Isso garante que nomeBusca nunca seja null, eliminando o aviso CS8600.*/
+        string nomeBusca = Console.ReadLine() ?? string.Empty;
+
+        bool encontrado = false;
+        foreach (string nome in nomes)
+        {
+            if (string.Equals(nome, nomeBusca, StringComparison.OrdinalIgnoreCase))
+            {
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (encontrado)
+        {
+            Console.WriteLine("O nome está na lista.");
+        }
+        else
+        {
+            Console.WriteLine("O nome não está na lista.");
+        }
+
+    }
+}
