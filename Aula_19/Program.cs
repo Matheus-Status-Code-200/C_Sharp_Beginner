2fc67bd160fb13da6ac41658c0d9581964ac700f
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-18 20:59:05 -0300

    Usando DateTime retorna tempo de estudo.

---
+++ Aula_19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\nPrograma usando DateTime.");
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Este programa retorma o tempo de estudo em C#.\n");
+        DateTime dataInicial = new DateTime(2023, 7, 13); // Definindo a data inicial
+
+        DateTime dataAtual = DateTime.Now; // Obtendo a data atual
+
+        TimeSpan tempoAprendizado = dataAtual - dataInicial; // Calculando o tempo de aprendizado
+
+        int diasAprendizado = tempoAprendizado.Days; // Obtendo o número de dias de aprendizado
+
+        Console.WriteLine("Data de inicio: " + dataInicial.ToString("dd/MM/yyyy"));
+        Console.WriteLine("Dia atual: " + dataAtual.ToString("dd/MM/yyyy"));
+        Console.WriteLine("Tempo de aprendizado em C#: " + diasAprendizado + " dias.");
+    }
+}
