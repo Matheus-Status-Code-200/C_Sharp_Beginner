ffe655c5dceb270b1dd53ce96ea2b3003a0d3980
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-22 13:15:39 -0300

    Inicio de estudo de classes.

--- Aula_24/Program.cs
+++ Aula_24/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+class Calculator
+{
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\nBem-vindo à calculadora!");
+        Console.WriteLine("-----------------------------\n");
+        Console.WriteLine("Usaremos classes separadas.\n");
+
+        Console.Write("Digite o primeiro número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Calculator calculator = new Calculator();
+        int result = calculator.Add(num1, num2);
+
+        Console.WriteLine($"Resultado da soma: {result}\n");
+    }
+}
