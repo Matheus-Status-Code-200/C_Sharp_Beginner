958371698770bf9bb87c2aba108212c95729d308
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-22 14:38:51 -0300

    Try e Catch na sequencia de Fibonacci.

--- Aula_26/Program.cs
+++ Aula_26/Program.cs
@@ -1,7 +1,43 @@
-<<<<<<< HEAD
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-=======
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
->>>>>>> 2864a65f77cfbc8754c16d1d39216c7017cf4526
+using System;
+
+class Program
+{
+    static int Fibonacci(int n)
+    {
+        if (n <= 0)
+            throw new ArgumentException("O número deve ser maior que zero.");
+
+        if (n == 1 || n == 2)
+            return 1;
+
+        int prev1 = 1;
+        int prev2 = 1;
+        int result = 0;
+
+        for (int i = 3; i <= n; i++)
+        {
+            result = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = result;
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Calculadora de Fibonacci");
+        Console.Write("Digite o valor de 'n' para calcular o n-ésimo número da sequência de Fibonacci: ");
+        int n = int.Parse(Console.ReadLine());
+
+        try
+        {
+            int fibonacciNumber = Fibonacci(n);
+            Console.WriteLine($"O {n}-ésimo número da sequência de Fibonacci é: {fibonacciNumber}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
